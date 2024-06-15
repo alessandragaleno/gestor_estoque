1aa01a9bba3db5de5a6333aa83bda72c4fe5da18
alessandragaleno <alessandragaleno421@gmqil.com>
2024-06-14 21:11:49 -0300

    classes adicionadas, e arquivo base_models e criação da pasta utils

--- produtos/models.py
+++ produtos/models.py
@@ -1,37 +1,59 @@
 from django.db import models
+from utils.base_models import BaseModel
 
 # Create your models here.
-class Fornecedor(models.Model):
-    nome_social = models.CharField(max_length=100, verbose_name='razão social do fornecedor')
-    nome_fantasia = models.CharField('nome fantasia do fornecedor')
-    produtos = models.ManyToManyField('produtos.Produto', verbose_name='Produtos do Fornecedor', through='FornecedorProduto')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data de criação do fornecedor')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data da atualização do fornecedor')
+class Local(BaseModel):
+    TIPOS_DE_LOCAL = [
+        ('f', 'Fisico'),
+        ('D', 'Digital'),
+    ]
+    nome = models.CharField(max_length=50,  verbose_name='Nome do local armazenado')
+    tipo = models.CharField(max_length=1,choices=TIPOS_DE_LOCAL,verbose_name='Tipo do local movimentado')
 
     class Meta:
-        tb_table = 'fornecedores'
+        db_table = 'locais'
 
-class FornecedorProduo(models.Model):
-    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, verbose_name='Produto do fornecedor')
-    preco = models.DecimalField(max_digits=10, decimal_place=2, verbose_name='Preço do produto')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data de criação')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data da atualização')
+class Movimentacao(BaseModel):
+    TIPOS_MOVIMENTACAO = [
+        ('1', 'Entrada'),
+        ('-1', 'Saída'),
+    ]
+    produto = models.ForeignKey('produtos.Produto',on_delete=models.CASCADE,verbose_name='Produto da movimentação')
+    quantidade = models.DecimalField(max_digits=10, decimal_places=6, verbose_name='Quantidad movimentada')
+    fornecedor = models.ForeignKey('produtos.Fornecedor', on_delete=models.CASCADE, verbose_name='Fornecedor do produto movimentado')
+    local = models.ForeignKey('produtos.local', on_delete=models.CASCADE, verbose_name='ocal de movimentação')
+    tipo = models.IntegerField(choices=TIPOS_MOVIMENTACAO,verbose_name='Tipo de movimentação')
 
-class Produto(models.Model):
+    class Meta:
+        db_table = 'movimentacoes'
+
+class Embalagem(BaseModel):
+    name = models.CharField(max_length=50, verbose_name='Nome da embalagem')
+    sigla = models.CharField(max_length=3, verbose_name='Sigla da embalagem')
+
+    class Meta:
+        db_table = 'embalagens'
+
+class Fornecedor(BaseModel):
+    nome_social = models.CharField(max_length=100, verbose_name='razão social do fornecedor')
+    nome_fantasia = models.CharField(max_length=50, verbose_name='nome fantasia do fornecedor')
+    produtos = models.ManyToManyField('produtos.Produto', verbose_name='Produtos do Fornecedor',)
+
+    class Meta:
+        db_table = 'fornecedores'
+
+class Produto(BaseModel):
     nome = models.CharField(max_length=100, verbose_name='nome do produto')
     categoria = models.ForeignKey('produtos.Categoria', on_delete=models.CASCADE, verbose_name='categoria de produto')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data de criação da categoria')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data da atualização da categoria')
+    embalagens = models.ManyToManyField('produtos.Embalagem', verbose_name='Embalagem do produto')
 
     class Meta:
-        tb_table = 'produtos'
+        db_table = 'produtos'
 
-class Categoria(models.Model):
+class Categoria(BaseModel):
     nome = models.CharField(max_length=100,  verbose_name='nome da categoria')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data de criação da categoria')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data da atualização da categoria')
     unique=True
 
     class Meta:
-        tb_table = 'categorias'
+        db_table = 'categorias'
 
