6dfbba500605246f361b37a002648b0c7ec442a0
alessandragaleno <alessandragaleno421@gmail.com>
2024-06-15 19:50:55 -0300

    Revisão de código, gestão_estoque

--- produtos/models.py
+++ produtos/models.py
@@ -4,11 +4,11 @@ from utils.base_models import BaseModel
 # Create your models here.
 class Local(BaseModel):
     TIPOS_DE_LOCAL = [
-        ('f', 'Fisico'),
+        ('F', 'Fisico'),
         ('D', 'Digital'),
     ]
-    nome = models.CharField(max_length=50,  verbose_name='Nome do local armazenado')
-    tipo = models.CharField(max_length=1,choices=TIPOS_DE_LOCAL,verbose_name='Tipo do local movimentado')
+    nome = models.CharField(max_length=50, verbose_name='Nome do local armazenado')
+    tipo = models.CharField(max_length=1, choices=TIPOS_DE_LOCAL, verbose_name='Tipo do local movimentado')
 
     class Meta:
         db_table = 'locais'
@@ -19,10 +19,10 @@ class Movimentacao(BaseModel):
         ('-1', 'Saída'),
     ]
     produto = models.ForeignKey('produtos.Produto',on_delete=models.CASCADE,verbose_name='Produto da movimentação')
-    quantidade = models.DecimalField(max_digits=10, decimal_places=6, verbose_name='Quantidad movimentada')
+    quantidade = models.DecimalField(max_digits=10, decimal_places=6, verbose_name='Quantidade movimentada')
     fornecedor = models.ForeignKey('produtos.Fornecedor', on_delete=models.CASCADE, verbose_name='Fornecedor do produto movimentado')
-    local = models.ForeignKey('produtos.local', on_delete=models.CASCADE, verbose_name='ocal de movimentação')
-    tipo = models.IntegerField(choices=TIPOS_MOVIMENTACAO,verbose_name='Tipo de movimentação')
+    local = models.ForeignKey('produtos.local', on_delete=models.CASCADE, verbose_name='Local de movimentação')
+    tipo = models.IntegerField(choices=TIPOS_MOVIMENTACAO, verbose_name='Tipo de movimentação')
 
     class Meta:
         db_table = 'movimentacoes'
@@ -35,15 +35,15 @@ class Embalagem(BaseModel):
         db_table = 'embalagens'
 
 class Fornecedor(BaseModel):
-    nome_social = models.CharField(max_length=100, verbose_name='razão social do fornecedor')
-    nome_fantasia = models.CharField(max_length=50, verbose_name='nome fantasia do fornecedor')
-    produtos = models.ManyToManyField('produtos.Produto', verbose_name='Produtos do Fornecedor',)
+    nome_social = models.CharField(max_length=100, verbose_name='Razão social do fornecedor')
+    nome_fantasia = models.CharField(max_length=50, verbose_name='Nome fantasia do fornecedor')
+    produtos = models.ManyToManyField('produtos.Produto', verbose_name='Produtos do Fornecedor')
 
     class Meta:
         db_table = 'fornecedores'
 
 class Produto(BaseModel):
-    nome = models.CharField(max_length=100, verbose_name='nome do produto')
+    nome = models.CharField(max_length=100, verbose_name='Nome do produto')
     categoria = models.ForeignKey('produtos.Categoria', on_delete=models.CASCADE, verbose_name='categoria de produto')
     embalagens = models.ManyToManyField('produtos.Embalagem', verbose_name='Embalagem do produto')
 
@@ -51,7 +51,7 @@ class Produto(BaseModel):
         db_table = 'produtos'
 
 class Categoria(BaseModel):
-    nome = models.CharField(max_length=100,  verbose_name='nome da categoria')
+    nome = models.CharField(max_length=100, verbose_name='Nome da categoria')
     unique=True
 
     class Meta:
