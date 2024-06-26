be767ad339ff617723b904cfd0d696ac83355a1b
alessandragaleno <alessandragaleno421@gmail.com>
2024-06-26 13:31:14 -0300

    correção de views

--- produtos/views.py
+++ produtos/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 
 
 def inicio(request):
-<<<<<<< HEAD
-    return render(request, 'produtos/base.html')
-=======
     return render(request, 'index.html')
->>>>>>> ebb2754a4badff8632d1060b6e127cc322ce5bbb
