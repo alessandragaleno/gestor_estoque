66d9aa9ef0531fcc07907491e54a1185e897f5c5
alessandragaleno <alessandragaleno421@gmqil.com>
2024-06-26 21:45:57 -0300

    adição de arquivos, e views

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import inicio
+from .views import adicionar_local, inicio, listar_locais
 
 urlpatterns = [
     path('', inicio, name='inicio'),
+    #locais
+    path('locais/', listar_locais, name='listar_locais'),  # noqa: F821
+    path('locais/adicionar/', adicionar_local, name='adicionar_local'),
+    #path('locais/editar/<int:pk>/', editar_local, name='editar_local'),
+    #path('locais/excluirr/<int:pk>/', excluir_local, name='excluir_local'),
+
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import LocalForm
+from .models import Local
 
 
 def inicio(request):
     return render(request, 'index.html')
+
+
+def listar_locais(request):
+    consulta = request.GET.get('q')
+    tipo = request.GET.get('tipo')
+    locais = Local.objects.all()  # noqa: F821
+    if consulta:
+        locais = locais.filter(nome_icontains=consulta)
+    if tipo:
+        locais = locais.filter(tipo=tipo)
+    return render(request, 'produtos/listar_locais.html', {'locais': locais})  # noqa: E501
+
+
+def adicionar_local(request):
+    if request.method == 'POST':
+        form = LocalForm(request.POST)
+        if form.is_valid():
+            return redirect('listar_locais')  # noqa: E112
+    else:
+        form = LocalForm()
+    return render(request, 'produtos/adicionar_local.html', {'form': form})
