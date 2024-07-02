2c41ce89a034ec9950be715323950fb439f979d5
alessandragaleno <alessandragaleno421@gmqil.com>
2024-07-01 21:31:44 -0300

    criação de htmls, e atualizações de views, urls, forms

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import path  # noqa: I001
+from .views import adicionar_local, inicio, listar_locais, listar_embalagem, adicionar_embalagem, editar_local  # noqa: E501, F811, F401
 
-from .views import adicionar_local, inicio, listar_locais
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    #locais
     path('locais/', listar_locais, name='listar_locais'),  # noqa: F821
     path('locais/adicionar/', adicionar_local, name='adicionar_local'),
-    #path('locais/editar/<int:pk>/', editar_local, name='editar_local'),
-    #path('locais/excluirr/<int:pk>/', excluir_local, name='excluir_local'),
-
+    path('locais/editar/<int:pk>/', editar_local, name='editar_local'),  # noqa: F821 # type: ignore
+    path('locais/excluirr/<int:pk>/', excluir_local, name='excluir_local'),  # type: ignore  # noqa: F821
+    # embalagens
+    path('embalagens/', listar_embalagem, name='listar_embalagens'),  # noqa: F821
+    path('embalagens/adicionar/', adicionar_embalagem, name='adicionar_embalagem'),  # noqa: E501
+    path('embalagens/editar/<int:pk>/', editar_embalagem, name='editar_embalagem'),  # noqa: F821 , E501# type: ignore
+    path('embalagens/excluir/<int:pk>/', excluir_embalagem, name='excluir_embalagem'),  # type: ignore  # noqa: F821, E501
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -27,3 +27,32 @@ def adicionar_local(request):
     else:
         form = LocalForm()
     return render(request, 'produtos/adicionar_local.html', {'form': form})
+
+
+def editar_local(request):
+    return render(request, 'produtos/editar_locais.html')
+
+
+def excluir_local(request):
+    return render(request, 'produtos/excluir_locais.html')
+
+
+def listar_embalagem(request):
+    consulta = request.GET.get('q')
+    tipo = request.GET.get('tipo')
+    locais = Local.objects.all()  # noqa: F821
+    if consulta:
+        locais = locais.filter(nome_icontains=consulta)
+    if tipo:
+        locais = locais.filter(tipo=tipo)
+    return render(request, 'produtos/listar_embalagem.html', {'locais': locais})  # noqa: E501
+
+
+def adicionar_embalagem(request):
+    if request.method == 'POST':
+        form = LocalForm(request.POST)
+        if form.is_valid():
+            return redirect('listar_embalagem')  # noqa: E112
+    else:
+        form = LocalForm()
+    return render(request, 'produtos/adicionar_embalagem.html', {'form': form})  # noqa: E501
