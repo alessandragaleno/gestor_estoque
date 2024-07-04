19a0d11ee4736d82bb90a6bd8383b02785cb778d
alessandragaleno <alessandragaleno421@gmqil.com>
2024-07-03 21:34:51 -0300

    correção de erros em tudo

--- produtos/forms.py
+++ produtos/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from .models import Local
+from .models import Local, Embalagem
 
 
 class LocalForm(forms.ModelForm):
     class Meta:
         model = Local
         fields = ['nome', 'tipo']
+
+class EmbalagemForm(forms.ModelForm):
+    class Meta:
+        model = Embalagem
+        fields = ['name', 'sigla']
